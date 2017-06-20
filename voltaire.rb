b1d56c2df7f4d0fc4110115c4a5d0383b6266044
kei <kei@omise.co>
2017-06-20 16:42:30 +0900

    aaa

--- voltaire.rb
+++ voltaire.rb
@@ -3,7 +3,7 @@ require "formula"
 class Voltaire < Formula
   homepage "https://github.com/keitaroemotion/voltaire"
   url "https://github.com/keitaroemotion/voltaire/archives/v0.1.tar.gz"
-  sha256 "f27aead1fce5533fedfdb796a4c6caa8712cbeaabd94a9e5b942de18329a8a49"
+  sha256 "8cd8c0589218be4336f87b9278716e9a8430f075"
   head "https://github.com/keitaroemotion/voltaire.git"
   version "0.1"
 
